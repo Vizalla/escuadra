2a69015d7d8729fd0014808a78c3cf159a7ed681
Jhaf <floresfernandezjonathanaugusto@gmail.com>
2026-05-13 00:06:11 -0400

    feat(geometría): se arreglo los casos de prueba de la implementacion de calculo de areas - Closes #138

--- src/escuadra/modulos/geometria/calculo_area.py
+++ src/escuadra/modulos/geometria/calculo_area.py
@@ -6,6 +6,3 @@ def area_circulo(radio):
 
 def area_rectangulo(base, altura):
     return base * altura
-print("el area del triangulo es:", area_triangulo(2, 4))
-print("el area del circulo es:", area_circulo(4))
-print("el area del rectangulo es:", area_rectangulo(58, 46))
